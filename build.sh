9222dac2c9f544689e574a04831308883818cf89
Kostyantyn Movchan <bsdkom@gmail.com>
2021-10-20 17:13:24 +0300

    fixed s3 copy build script

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
 
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep moodle-tf |tail -n1 |cut -d ' ' -f3`
-aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
+aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
