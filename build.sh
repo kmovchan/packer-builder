eddc1b61293f98608c3df63ece3c85d6237259e2
Kostyantyn Movchan <bsdkom@gmail.com>
2021-10-20 17:06:41 +0300

    add build script to packer for moodle

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+AWS_REGION="eu-west-2"
+ARTIFACT=`packer build -machine-readable moodle.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+
+AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
+
+S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep moodle-tf |tail -n1 |cut -d ' ' -f3`
+aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
