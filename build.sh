bd337cd5883be656f3470a99657d4ebd2c2e04e2
Kostyantyn Movchan <bsdkom@gmail.com>
2021-10-21 12:16:11 +0300

    change buid.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-AWS_REGION="eu-west-2"
-ARTIFACT=`packer build -machine-readable moodle.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-
+AWS_REGION="eu-central-1"
+echo "Staring build moodle  ami instance"
+if packer build -machine-readable moodle.json > out.txt
+ then 
+ARTIFACT=$( cat out.txt |awk -F, '$0 ~/artifact,0,id/ {print $6}' )
+ else 
+  echo "Packer can't compile ami for moodle" && exit 1
+fi
 AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+echo "Saving variable to variables.tf"
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
 
+echo "Copy variable.tf to S3 bucket"
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep moodle-tf |tail -n1 |cut -d ' ' -f3`
 aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
